cc0158aa1663e6b90ca33aab11d1c9eae9becf96
Patrick Widauer <a__ctor@hotmail.com>
2016-12-07 20:08:16 +0100

    Fixed last commit.

--- src/Evaders/Services/Extensions.cs
+++ src/Evaders/Services/Extensions.cs
@@ -1,7 +1,12 @@
 ﻿namespace Evaders.Services
 {
     using System;
+    using System.Linq;
+    using JetBrains.Annotations;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Server;
     using Server.Integration;
 
     public static class Extensions
@@ -9,6 +14,86 @@
         public static IProviderFactory<T> GetRequiredProvider<T>(this IServiceProvider services)
         {
             return services.GetRequiredService<IProviderFactory<T>>();
+        }
+
+
+        public static void ApplySetting([NotNull] this ServerSettings value, [NotNull] string key, [NotNull] IConfiguration configuration, ILogger logger, [NotNull] Action<ServerSettings, string> action)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var data = configuration.GetValue<string>(key);
+            if (data != null)
+            {
+                action(value, data);
+            }
+            else
+            {
+                logger.LogWarning($"Configuration value not loaded: '{key}'");
+            }
+        }
+
+        public static void ApplySettingCustom([NotNull] this ServerSettings value, [NotNull] string key, [NotNull] IConfiguration configuration, ILogger logger, [NotNull] Func<ServerSettings, string, bool> action)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var data = configuration.GetValue<string>(key);
+            if (data == null || !action(value, data))
+            {
+                logger.LogWarning($"Configuration value not loaded: '{key}'");
+            }
+        }
+
+        public static void ApplySettingArray<TData>([NotNull] this ServerSettings value, [NotNull] string key, [NotNull] IConfiguration configuration, [NotNull] Action<ServerSettings, TData[]> action)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action));
+            }
+
+            var section = configuration.GetSection(key);
+            action(value, section.GetChildren().Select(e => section.GetValue<TData>(e.Key)).ToArray());
         }
     }
 }
--- src/Evaders/Services/GameManager.cs
+++ src/Evaders/Services/GameManager.cs
@@ -1,7 +1,9 @@
 ﻿namespace Evaders.Services
 {
     using System;
-    using System.Linq;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Threading.Tasks;
     using Core.Game;
     using JetBrains.Annotations;
     using Microsoft.AspNetCore.Hosting;
@@ -18,7 +20,7 @@
 
 
         /* Server */
-        EvadersServer _server;
+        private EvadersServer _server;
 
         /* Server mechanics */
         private IProviderFactory<IMatchmaking> _matchmakingProvider;
@@ -51,39 +53,115 @@
             /*
              * CONFIGURE serversettings.json
              */
+            {
+                var configurationBuilder = new ConfigurationBuilder();
+                configurationBuilder.SetBasePath(hostingEnvironment.ContentRootPath);
+                configurationBuilder.AddJsonFile("serversettings.json", false, true);
+
+                var root = configurationBuilder.Build();
+                OnServerSettingsReload(root);
+            }
 
             /*
              * CONFIGURE gamesettings.json 
              */
-            var configurationBuilder = new ConfigurationBuilder();
-            configurationBuilder.SetBasePath(hostingEnvironment.ContentRootPath);
-            configurationBuilder.AddJsonFile("gamesettings.json", false, true);
-
-            var configurationRoot = configurationBuilder.Build();
-            RegisterReload(configurationRoot);
-        }
-
-        private void RegisterReload(IConfigurationRoot root)
-        {
-            var reloadToken = root.GetReloadToken();
-            if (reloadToken.HasChanged)
             {
-                _logger.LogWarning("nope");
-                throw new NotSupportedException();
-            }
-            reloadToken.RegisterChangeCallback(OnGamesettingsReload, root);
-            if (reloadToken != root.GetReloadToken())
-            {
-                _logger.LogError("noooooo");
+                var configurationBuilder = new ConfigurationBuilder();
+                configurationBuilder.SetBasePath(hostingEnvironment.ContentRootPath);
+                configurationBuilder.AddJsonFile("gamesettings.json", false, true);
+
+                var root = configurationBuilder.Build();
             }
         }
 
-        private void OnGamesettingsReload(object data)
-        {
-            var root = (IConfigurationRoot) data;
-            RegisterReload(root);
 
-            _logger.LogWarning("configuration reloaded!");
+        private static ServerSettings LoadServerSettings(IConfiguration root, ILogger logger)
+        {
+            var settings = new ServerSettings();
+
+            // game specific settings
+            settings.ApplySettingArray<string>("game:modes", root, (s, e) => s.GameModes = e);
+
+            // general settings
+            settings.ApplySetting("general:motd", root, logger, (s, e) => s.Motd = e);
+            settings.ApplySettingCustom("general:maxUsernameLength", root, logger, (s, e) =>
+            {
+                int length;
+                if (!int.TryParse(e, out length) || length < 1)
+                    return false;
+                s.MaxUsernameLength = length;
+                return true;
+            });
+
+            // network settings
+            settings.ApplySettingCustom("networking:ip", root, logger, (s, e) =>
+            {
+                IPAddress address;
+                if (!IPAddress.TryParse(e, out address))
+                    return false;
+                s.IP = address;
+                return true;
+            });
+            settings.ApplySettingCustom("networking:port", root, logger, (s, e) =>
+            {
+                ushort port;
+                if (!ushort.TryParse(e, out port))
+                    return false;
+                s.Port = port;
+                return true;
+            });
+
+            // queue settings
+            settings.ApplySettingCustom("queue:maxTime", root, logger, (s, e) =>
+            {
+                float maxTime;
+                if (!float.TryParse(e, out maxTime) || !(maxTime > 0f))
+                    return false;
+                s.MaxTimeInQueueSec = maxTime;
+                return true;
+            });
+
+            return settings;
+        }
+
+        private static Dictionary<string, GameSettings> LoadGameSettings(IConfiguration root, ILogger logger)
+        {
+            return null;
+        }
+
+
+        private static void RegisterReload([NotNull] IConfigurationRoot root, Action<IConfigurationRoot> target)
+        {
+            if (root == null)
+                throw new ArgumentNullException(nameof(root));
+            root.GetReloadToken().RegisterChangeCallback(e => target((IConfigurationRoot) e), root);
+        }
+
+
+        private void OnServerSettingsReload(IConfigurationRoot root)
+        {
+            // register again
+            RegisterReload(root, OnServerSettingsReload);
+
+            // load the settings
+            var settings = LoadServerSettings(root, _logger);
+            // todo set server settings
+
+            // log that the setting have been reloaded
+            _logger.LogInformation("Server settings reloaded.");
+        }
+
+        private void OnGameSettingsReload(IConfigurationRoot root)
+        {
+            // register again
+            RegisterReload(root, OnGameSettingsReload);
+
+            // load the settings
+            var settings = LoadGameSettings(root, _logger);
+            // todo set game settings
+
+            // log that the settings have been reloaded
+            _logger.LogInformation("Game settings reloaded.");
         }
     }
 }
